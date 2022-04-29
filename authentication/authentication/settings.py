61af42c9885728d8a35dd104de17d5cf1e78f8bc
rashminainwal01 <rashmi.nainwal@kiwitech.com>
2022-04-29 18:05:37 +0530

    new commit

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -127,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER"),
+EMAIL_HOST_PASSWORD =os.getenv( "EMAIL_HOST_PASSWORD"),
+EMAIL_PORT = os.getenv("EMAIL_PORT"),
